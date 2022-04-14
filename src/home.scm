b402f222833571f36b95aa9a0d2887277e375d00
Tim Whitaker <camp4climber@gmail.com>
2022-04-14 11:34:14 -0700

    Add git attributes

--- src/home.scm
+++ src/home.scm
@@ -47,7 +47,7 @@
               (div 
                 (h1 "Quantum Neuron Selection")
                 (p "Gradient descent methods have long been the de facto standard for training deep neural networks. Millions of training samples are fed into models with billions of parameters, which are slowly updated over hundreds of epochs. Recently, it's been shown that large, randomly initialized, neural networks contain subnetworks that perform as well as fully trained models. This insight offers a promising avenue for...")
-                (a (@ (href "#") (class "read-more")) "Read More"))
+                (a (@ (href "/static/quantum-neuron-selection.pdf") (class "read-more")) "Read More"))
             )
           )
           (div (@ (class "there-is-no-magic-subnetwork"))
@@ -55,7 +55,7 @@
               (div 
                 (h1 "There Is No Magic Subnetwork")
                 (p "Diversity is an important consideration in the construction of robust neural network ensembles. A collection of well trained models will generalize better to unseen data if they are diverse in the patterns they respond to and the predictions they make. Encouraging diversity becomes especially important for low-cost ensemble methods, as members often share network structure or training epochs in order to avoid training several independent networks from scratch...")
-                (a (@ (href "#") (class "read-more")) "Read More"))
+                (a (@ (href "/static/there-is-no-magic-subnetwork.pdf") (class "read-more")) "Read More"))
             )
           )
           (div (@ (class "prune-and-tune-ensembles"))
@@ -63,7 +63,7 @@
               (div 
                 (h1 "Prune and Tune Ensembles")
                 (p "Ensemble Learning is an effective method for improving generalization in machine learning. However, as state-of-the-art neural networks grow larger, the computational cost associated with training several independent networks becomes expensive. We introduce a fast, low-cost method for creating diverse ensembles of neural networks without needing to train multiple models from scratch. We do this by first training a single parent network. We then create child networks by cloning the parent and...")
-                (a (@ (href "#") (class "read-more")) "Read More"))
+                (a (@ (href "/static/prune-and-tune-ensembles.pdf") (class "read-more")) "Read More"))
             )
           )
           
