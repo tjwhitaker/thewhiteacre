7ecbaf6c6d8b81c0b52a3c02d2b6d5062cc92d4d
Tim Whitaker <camp4climber@gmail.com>
2022-06-09 20:02:14 -0700

    Update lenet

--- src/architectures.scm
+++ src/architectures.scm
@@ -70,14 +70,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class Net(nn.Module):
+class LeNet(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(6, 16, 5)
-        self.conv2 = nn.Conv2d(16, 32, 5)
-        self.conv3 = nn.Conv2d(120, 120, 5)
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
         self.pool = nn.MaxPool2d(kernel_size=2)
-        self.fc1 = nn.Linear(120, 120)
+        self.fc1 = nn.Linear(256, 120)
         self.fc2(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
@@ -86,7 +85,8 @@ class Net(nn.Module):
         x = self.pool(F.relu(self.conv2(x)))
         x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         return x")))))
               
               (div (@ (class "model"))
